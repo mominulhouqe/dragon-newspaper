9a14c9bd1229b31e885d64ab8ffb5967ebe934ac
mominul <ulhouqemomin39@gmail.com>
2023-04-24 19:07:26 +0600

    added time

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h4>This is heder</h4>
+            <div className="text-center">
+                <img src={logo} alt="" />
+                <p className='text-center'> <small>Journalism Without Fear or Favor</small></p>
+                <p>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a") }</p>
+            </div>
             
         </div>
     );
