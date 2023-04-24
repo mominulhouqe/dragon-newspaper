608cef03fee46dc5418129062116ccd6fbc5bff0
mominul <ulhouqemomin39@gmail.com>
2023-04-24 23:55:20 +0600

    left navber added

--- src/pages/Shared/leftNav/LeftNav.jsx
+++ src/pages/Shared/leftNav/LeftNav.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const leftNav = () => {
+const LeftNav = () => {
+
+    const [categories, setCategories] = useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/categories')
+        .then(res => res.json())
+        .then(data =>setCategories(data))
+        .catch(error => console.log(error))
+    }, [])
+
     return (
         <div>
-            <h4>This is Left Nav bar</h4>
+            <h4>All Categories hi</h4>
+            
+            <div className=''>
+                {
+                    categories.map(category => <p key={category.id}>
+                        <Link to={`/${category.id}`}>{category.name}</Link>
+                    </p>)
+                }
+            
+            </div>
         </div>
     );
 };
 
-export default leftNav;
+export default LeftNav;
